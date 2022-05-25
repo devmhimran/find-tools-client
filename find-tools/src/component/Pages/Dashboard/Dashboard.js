6e5a35a502c5559b74247877490bd6a49177b603
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-25 17:53:46 +0600

    Update Dashboard Nav

--- find-tools/src/component/Pages/Dashboard/Dashboard.js
+++ find-tools/src/component/Pages/Dashboard/Dashboard.js
@@ -5,13 +5,13 @@ const Dashboard = () => {
     return (
         <div className="container mx-auto">
             <div class="drawer drawer-mobile">
-                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+                <input id="dashboard-drawer" type="checkbox" class="drawer-toggle" />
                 <div class="drawer-content  bg-slate-100  p-5 m-2 lg:p-10 rounded-lg">
-                    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    
                     <Outlet></Outlet>
                 </div>
                 <div class="drawer-side border rounded-lg m-2">
-                    <label for="my-drawer-2" class="drawer-overlay"></label>
+                    <label for="dashboard-drawer" class="drawer-overlay"></label>
                     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                         <li><Link className='border-b' to='/dashboard/'>Manage All Orders</Link></li>
                         <li><Link className='border-b' to='/dashboard/manageproducts'>Manage Products</Link></li>
--- find-tools/src/component/Pages/Home.js/Navbar.js
+++ find-tools/src/component/Pages/Home.js/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
                                     </label>
                                     <ul tabIndex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                                         <li>
-                                            <p to='/' className="justify-between capitalize">
+                                            <p to='/' className="justify-between capitalize font-bold">
                                                 {
                                                     user ? user.displayName : ''
                                                 }
@@ -68,6 +68,9 @@ const Navbar = () => {
                             <Link className="btn btn-secondary" to='/signin'>Sign in</Link>
 
                     }
+                    <label for="dashboard-drawer" tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
                 </div>
             </div>
 
