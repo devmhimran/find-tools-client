a0e2e3c3e350809172b5dbe5289e431ca235a619
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-26 17:35:48 +0600

    Update Loading

--- find-tools/src/component/Pages/Loading/Loading.js
+++ find-tools/src/component/Pages/Loading/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { RotatingLines } from 'react-loader-spinner';
+import './Loading.css';
 
 const Loading = () => {
     return (
-        <div className='flex justify-center'>
+        <div className='absolute left-1/2 top-1/2 preloader'>
             <RotatingLines width="100" strokeColor="#a991f7" strokeWidth="1"
                 animationDuration="2" />
         </div>
