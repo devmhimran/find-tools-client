c74793d467be149d605250cb8dc67ff5f21cfc93
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-28 03:16:41 +0600

    Update Product Card

--- find-tools/src/component/Pages/AllProducts/ProductData.js
+++ find-tools/src/component/Pages/AllProducts/ProductData.js
@@ -1,27 +1,30 @@
 import React from 'react';
 
-const ProductData = ({products}) => {
-    const {productName, productImage, productDescription, productQuantity, productPrice} = products;
-    return (
-        // <div className="card drop-shadow-lg rounded-xl">
-        //     <div className="card-body">
-        //         {productName}
-        //         <img src={productImage} alt="" />
-        //         {productDescription}
-        //         {productQuantity}
-        //         {productPrice}
-        //     </div>
-        // </div>
-        <div className="card w-full bg-base-100 shadow-xl">
-        <div className="card-body">
-          <img src={productImage} alt="" />
-          <h2 className='text-xl font-semibold'>{productName}</h2>
-          <p className='text-base my-3'>{productDescription.slice(0, 45)}[...]</p>
-          <p className='text-base my-3'>$ {productPrice}</p>
-            <button className="btn btn-primary">Buy Now</button>
+const ProductData = ({ products }) => {
+  const { productName, productImage, productDescription, productQuantity, productPrice } = products;
+  return (
+    // <div className="card drop-shadow-lg rounded-xl">
+    //     <div className="card-body">
+    //         {productName}
+    //         <img src={productImage} alt="" />
+    //         {productDescription}
+    //         {productQuantity}
+    //         {productPrice}
+    //     </div>
+    // </div>
+    <div className="card w-full bg-base-100 shadow-xl">
+      <div className="card-body">
+        <img src={productImage} alt="" />
+        <h2 className='text-xl font-semibold'>{productName}</h2>
+        <p className='text-base my-3'>{productDescription.slice(0, 45)}[...]</p>
+        <div className='flex justify-between'>
+          <p className='text-2xl mb-2 font-semibold'>${productPrice}</p>
+          <p className='text-lg mb-2 font-semibold text-right'>{productQuantity > 0 ? 'Available' : 'Not Available'}</p>
         </div>
+        <button className="btn btn-primary">Buy Now</button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ProductData;
