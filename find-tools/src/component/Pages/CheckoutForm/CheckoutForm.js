00205a557341946c539f7464d55c668eb9f1061f
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-29 19:40:33 +0600

    Update Checkout

--- find-tools/src/component/Pages/CheckoutForm/CheckoutForm.js
+++ find-tools/src/component/Pages/CheckoutForm/CheckoutForm.js
@@ -1,83 +1,105 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = ({order}) => {
+const CheckoutForm = ({ order }) => {
     const stripe = useStripe();
     const elements = useElements();
     const [cardError, setCardError] = useState('');
     const [clientSecret, setClientSecret] = useState('');
+    const [success, setSuccess] = useState('');
 
-    const {productPrice, quantity} = order;
+    const { productPrice, quantity, name, email } = order;
     const price = productPrice * quantity;
 
-    console.log(productPrice);
-    useEffect(()=>{
-        if(price){
+    useEffect(() => {
+        if (price) {
             fetch('http://localhost:5000/create-payment-intent', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-            },
-            body: JSON.stringify({price})
-        }).then(res => res.json())
-        .then(data=> {
-            if(data?.clientSecret){
-                setClientSecret(data.clientSecret);
-            }
-        })
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify({ price })
+            }).then(res => res.json())
+                .then(data => {
+                    if (data?.clientSecret) {
+                        setClientSecret(data.clientSecret);
+                    }
+                })
         }
-    },[price])
+    }, [price])
 
-    const handleSubmit = async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (!stripe || !elements) {
             // Stripe.js has not loaded yet. Make sure to disable
             // form submission until Stripe.js has loaded.
             return;
-          }
-          const card = elements.getElement(CardElement);
-          if (card == null) {
+        }
+        const card = elements.getElement(CardElement);
+        if (card == null) {
             return;
-          }
-          const {error, paymentMethod} = await stripe.createPaymentMethod({
+        }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card,
-          });
-      
-          if (error) {
+        });
+
+        if (error) {
             // console.log('[error]', error);
-            setCardError(error.message);
-          } else {
+            setCardError(error?.message);
+        } else {
             // console.log('[PaymentMethod]', paymentMethod);
             setCardError('');
-          }
+        }
+
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: name,
+                        email: email,
+                    },
+                },
+            },
+        );
+       if(intentError){
+        setCardError(intentError?.message);
+        setSuccess('');
+       }else{
+        setCardError('');
+        console.log(paymentIntent)
+        setSuccess('Congrats! Your is completed')
+       }
     }
     return (
         <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
-              },
-            },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <small className='text-red-500 '>{cardError}</small>
-      <div>
-      <button className='btn btn-primary mt-10' type="submit" disabled={!stripe || !clientSecret}>
-        Order
-      </button>
-      </div>
-    </form>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <small className='text-red-500 '>{cardError}</small>
+            <small className='text-green-500 '>{success}</small>
+            <div>
+                <button className='btn btn-primary mt-10' type="submit" disabled={!stripe || !clientSecret}>
+                    Order
+                </button>
+            </div>
+        </form>
     );
 };
 
