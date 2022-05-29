82764b1d3c5bd70517f31d3968b9dd51a1e6fe11
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-29 12:50:21 +0600

    Update My Portfolio

--- find-tools/src/component/Pages/MyPortfolio/MyPortolio.js
+++ find-tools/src/component/Pages/MyPortfolio/MyPortolio.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import portfolioHeroImage from '../../../Assets/imran-pic-b-w.png';
+import { FaPhp, FaBootstrap, FaWordpressSimple, FaElementor } from 'react-icons/fa';
+import { GrHtml5 } from 'react-icons/gr';
+import { DiCss3, DiReact } from 'react-icons/di';
+import { SiJavascript } from 'react-icons/si';
 
 const MyPortolio = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto'>
+            <div className="hero h-5/6 bg-black py-10 lg:py-20 px-4 lg:px-28 my-10 rounded-3xl">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={portfolioHeroImage} alt='' className="max-w-sm rounded-lg shadow-2xl w-full mx-auto" />
+                    <div>
+                        <h1 className="text-3xl lg:text-5xl font-bold text-white">Myself Mahmud Hasan Imran</h1>
+                        <p className="py-6 text-slate-400">I'm a Professional Web Developer</p>
+                    </div>
+                </div>
+            </div>
+            <div className="experience py-28">
+                <h1 className='text-2xl lg:text-5xl font-bold text-center'>Experience With</h1>
+                <div className="card shadow-xl w-10/12 lg:w-6/12 mx-auto mt-10">
+                    <div className="card-body">
+                        <div className="grid grid-cols-2 lg:grid-cols-4">
+                            <div className='text-5xl mx-auto p-3'>
+                                <GrHtml5></GrHtml5>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <DiCss3></DiCss3>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <FaBootstrap></FaBootstrap>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <FaPhp></FaPhp>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <SiJavascript></SiJavascript>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <DiReact></DiReact>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <FaWordpressSimple></FaWordpressSimple>
+                            </div>
+                            <div className='text-5xl  mx-auto p-3'>
+                                <FaElementor></FaElementor>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="educational">
+                <h1 className='text-2xl lg:text-5xl font-bold text-center'>Educational Background</h1>
+                <div class="hero py-14 lg:py-24 my-10 bg-slate-200 rounded-lg">
+                    <div class="hero-content flex-col lg:flex-row">
+                        <img src="https://www.bubt.edu.bd/assets/componats/new-image/BUBT-Logo.png" class="w-7/12 lg:w-80 rounded-lg" />
+                        <div className='ml-5'>
+                            <h1 class="text-5xl font-bold">Bsc. in CSE</h1>
+                            <p class="py-6">I've successfully complete Bsc. in CSE from Bangladesh University of Business and Technology (BUBT).
+                                Since 2018-2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
